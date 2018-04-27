1b0417e1d208c73307396035fbb96874218e3839
Jasmine1112 <wh298@cornell.edu>
2018-04-27 16:20:49 -0400

    added final version option

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -72,6 +72,31 @@ def search():
 			map_geo = map_geo,
 			version = system_version,
 			unsplashed_queries = queries)
+	elif system_version == "final":
+		# change this to the final final version of backend system
+		if not (location_query):
+			location_query = ""
+		if not (description_query):
+			description_query = ""
+		output_tupes = (location_query, description_query)
+
+		results = v2.getPlaces(output_tupes, max_distance)
+		queries = []
+		raw_country = ""
+		for result in results:
+			if(len(result[3]) > 0):
+				raw_country = result[3][0][1]
+				queries.append(raw_country)
+			else:
+				queries.append(None)
+		return render_template('search.html', activity_query = activity_query,
+			location_query = location_query,
+			description_query= description_query,
+			output_message = (output_tupes[0] == "" and output_tupes[1] == ""),
+			results = results,
+			map_geo = map_geo,
+			version = system_version,
+			unsplashed_queries = queries)
 	else:#this is the homepage render
 		if not (location_query):
 			location_query = ""
